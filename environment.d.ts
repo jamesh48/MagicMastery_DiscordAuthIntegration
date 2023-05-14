990b2094b0e235efb59eeaae25cc313a79f578c8
James Hrivnak <James.Hrivnak@united.com>
2023-05-14 16:48:30 -0600

    change environment.d.ts file

--- environment.d.ts
+++ environment.d.ts
@@ -2,12 +2,14 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV?: 'development' | 'production' | 'test';
+      ACM_CERTIFICATE_ARN: string;
+      ACTIVE_CAMPAIGN_API_TOKEN: string;
+      ACTIVE_CAMPAIGN_BASEURL: string;
       DISCORD_BOT_TOKEN: string;
       DISCORD_CLIENT_ID: string;
       DISCORD_CLIENT_SECRET: string;
       DISCORD_DEFAULT_CHANNELID: string;
       MM_AUTH_URL: string;
-      MM_GUILD_ID: string;
       EXPRESS_SERVER_PORT: string;
     }
   }
