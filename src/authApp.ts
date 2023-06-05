20c649a307627ce81e0752c7f114723ae15659fe
James Hrivnak <James.Hrivnak@united.com>
2023-06-05 13:21:44 -0600

    CLEANUP pt 2, remove oauth2.0 route

--- src/authApp.ts
+++ src/authApp.ts
@@ -18,48 +18,6 @@ app.get('/healthcheck', async (_req, res) => {
   res.send('App is Healthy!');
 });
 
-app.get('/authCode', async (req, res) => {
-  /* Parameters */
-  const code = req.query.code as string;
-  const client_secret = process.env.DISCORD_CLIENT_SECRET;
-  const client_id = process.env.DISCORD_CLIENT_ID;
-  const redirect_uri = `${process.env.MM_AUTH_URL}/authCode`;
-
-  const oAuthResult = await fetch('https://discord.com/api/oauth2/token', {
-    method: 'POST',
-    body: new URLSearchParams({
-      client_id,
-      client_secret,
-      code,
-      grant_type: 'authorization_code',
-      scope: 'email',
-      redirect_uri,
-    }),
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-  });
-
-  const oAuthData = await oAuthResult.json();
-
-  const userResult = await fetch('https://discord.com/api/users/@me', {
-    headers: {
-      authorization: `${oAuthData.token_type} ${oAuthData.access_token}`,
-    },
-  });
-
-  console.info('\n\n<--- USER RESULT --->\n\n');
-  const candidateUser = await userResult.json();
-  const { id: discordId, email } = candidateUser;
-  console.info(discordId, email);
-
-  if (!discordId) {
-    return res.status(400).send('bad request');
-  }
-
-  return res.status(200).send('ok');
-});
-
 app.post('/acmpActivate', jsonParser, async (req, res) => {
   // Validate that the inputs are present
   if (!req.body.discordId) {
